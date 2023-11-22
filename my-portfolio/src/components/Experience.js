d50dc7290f686c1e69dd937340d1b7d4500c6a71
Yskak <shiyap.yskak@gmail.com>
2023-11-22 13:46:14 +1100

    fixed cv

--- my-portfolio/src/components/Experience.js
+++ my-portfolio/src/components/Experience.js
@@ -7,8 +7,8 @@ const Experience = () => {
   return (
     <section id="experience" className={styles.experience}>
       <h2>Experience</h2>
-      <p>For a more professional look at my experience, here is <a href="/Yskak-Resume-2.pdf" target="_blank">my CV</a>.</p>
-      <PDFViewer src="/Yskak-Resume-2.pdf" />
+      <p>For a more professional look at my experience, here is <a href="/Users/yskakshiyap/Desktop/yskak.github.io/my-portfolio/public/Yskak-Resume-2.pdf" target="_blank">my CV</a>.</p>
+      <PDFViewer src="/Users/yskakshiyap/Desktop/yskak.github.io/my-portfolio/public/Yskak-Resume-2.pdf" />
     </section>
   );
 };
